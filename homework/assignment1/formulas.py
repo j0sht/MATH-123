cb3b7da3c422c1668cefd1e57549b7ee79b166e9
Joshua Tate <jmtate@icloud.com>
2018-05-10 20:02:24 -0700

    Changed q1c to include default argument

--- homework/assignment1/formulas.py
+++ homework/assignment1/formulas.py
@@ -28,10 +28,9 @@ def oddCubes(n):
 def q1b():
     return 3 * (sumToN(333) - sumToN(33))
 
-# Formula for Q1c (n = 250)
-def q1c(n):
+def q1c(n=250):
     return oddSquares(n) - evenSquares(n)
 
 if __name__ == "__main__":
     print("102 + 105 + 108 + ... + 999 =", q1b())
-    print("1^2 - 2^2 + 3^2 - 4^2 + 5^2 - 6^2 + ... + 499^2 - 500^2 =", q1c(250))
+    print("1^2 - 2^2 + 3^2 - 4^2 + 5^2 - 6^2 + ... + 499^2 - 500^2 =", q1c())
