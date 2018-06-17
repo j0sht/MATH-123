04f67293953319925f2e66a542ed6ac52fc5b52e
Joshua Tate <jmtate@icloud.com>
2018-06-16 17:40:43 -0700

    removed the_gcd variable, added comments and brackets to ternary operator

--- number-theory/euclid_gcd.py
+++ number-theory/euclid_gcd.py
@@ -1,11 +1,10 @@
 def gcd(n1, n2):
-    largest = n1 if n1 > n2 else n2
-    smallest = n2 if largest == n1 else n1
+    largest = n1 if (n1 > n2) else n2
+    smallest = n2 if (n1 == largest) else n1
     remainder = smallest
-    the_gcd = remainder
     while remainder != 0:
         factor = largest // smallest
-        remainder = largest - (smallest*factor)
+        remainder = largest - (smallest*factor) # == (largest % smallest)
         largest = smallest
         smallest = remainder
     return largest
